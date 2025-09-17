5abbcea860aa2c92875c7052f85d3aff7f83eccb
taniamajumder <taniamou1998march@gmail.com>
2025-09-17 10:59:32 +0000

    Add Dockerization files and setup

--- Mywebapp/backend/server.js
+++ Mywebapp/backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = 5001;
+const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -12,6 +12,6 @@ app.get("/api/hello", (req, res) => {
   res.send({ message: "Hello from Express backend 👋" });
 });
 
-app.listen(5001, "0.0.0.0", () => {
-  console.log("Backend running on http://0.0.0.0:5001" );
+app.listen(5000, "0.0.0.0", () => {
+  console.log("Backend running on http://0.0.0.0:5000" );
 });
